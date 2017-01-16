efcd281a90ea9b7ef13982735ea2c3af6f30c62d
pupo162 <pupo163@hotmail.com>
2017-01-16 00:09:49 +0000

    Finished reverse words challenge!

--- Reverse Words/main.c
+++ Reverse Words/main.c
@@ -13,26 +13,38 @@
 #include <stdlib.h>
 #include <string.h>   
 
-char** readfile(char** words, const char* inputfile, int*numberoflines);
-char*  reorganize(char *line);
+char** readfile  ( char **words,  char *inputfile,  int *numberoflines);
+int    writefile ( char **words,  char *outputfile, int *numberoflines);
+char*  reorganize( char *line);
 
-int main() {
+int main(int argc, char *argv[]) {
 
-    const char *inputfile = "B-small-practice.in";         // Allocates name of the read file
+    if (argv[1] == NULL)                                   // IF argument
+    {
+        printf("please pass a file name as argument");         // Complains if no argument provided
+        return 0;                                              // Finishes the program
+    }
+    else                                                   // Else
+    { 
+        printf("parsing: %s \n", argv[1] );                   // Starting program message
+    }
 
     char **words;                                          // Allocates an array of strings
 
-    int numberoflines;
-    words = readfile( words, inputfile, &numberoflines);   // Reads the array of strings from file
+    int numberoflines;                                     // Declares variable for number of lines
+    words = readfile( words, argv[1], &numberoflines);     // Reads the array of strings from file
 
-    if ( numberoflines < 1 ) return 0;                     // If no read lines finishes the program
+    if ( numberoflines < 1 ){
+        printf("error reading file! \n"); 
+        return 0; 
+    }                   
 
     for (int i = 0; i < numberoflines; i++)                // For every line read
     {
-        printf("before: %s \n", words[i] );    
-        reorganize(words[i]);                     // Print the line      
-        //printf("after: %s \n",  reorganize(words[i]) );                         // Print the line        
+        strcpy(words[i],reorganize(words[i]));
     }
+
+    writefile(words, argv[1], &numberoflines);
 
     return 0;
 }
@@ -41,46 +53,46 @@ int main() {
 // reorganizes a line by size of char
 char* reorganize(char *line) {
  
-    char **words; 
-    char *word;                                     
-    char *response;
+    char **words;                                             // Declare array with words
+    char *word;                                               // Declare variable for single word
+    char *response = malloc( strlen(line) * sizeof(char));    // Declare and allocate response string
 
-    int  numberofwords = 1; // Atleast one word in the list
-    int  i             = 0; // Loop variable
-    int  j             = 0; // Loop variable
+    int  numberofwords = 1;                                   // Atleast one word in the list
 
-    for ( i = 0; i < strlen(line); i++)
-    {
-        if(line[i] == ' ') numberofwords++;
+    for ( int i = 0; i < strlen(line); i++)                   // Count words based on spaces
+    { 
+        if(line[i] == ' ') numberofwords++;                       // Increment counter
     }
     
-    words = malloc( sizeof(char*) * (numberofwords + 1) );
-    
-    for( i = 0; i < numberofwords; i++)
+    words = malloc(numberofwords*sizeof(char *));             // Allocate memory based on number of words
+
+    for( int i = 0; i < numberofwords; i++)                   // For each word
     {   
-        word = strsep(&line, " ");
-        strcpy(words[j],word);  
-    }
+        word     = strsep(&line, " ");                            // Separete word base on spaces
+        words[i] = malloc(sizeof(char)*(strlen(word) + 1));       // Allocate memory for the word
+        strcpy(words[i],word);                                    // Copy word to array
+    }; 
 
-    strcpy(response, words[0]);
+    strcpy(response, words[numberofwords-1] );                // Copy last word to response
 
-    //printf("after: %s \n",  response );
-
-    /*
-    for (i = numberofwords -2; i > 0; i-- )
+    for (int i = numberofwords -2; i > -1; i-- )              // For every other word
     {
-        strcat(response," ");
-        strcat(response,words[i]);
-    } */
+        strcat(response," ");                                    // Add a space
+        strcat(response,words[i]);                               // Add a word
+    } 
 
-    return response;
+    return response;                                          // Return response
 }
 
 // Reads a file, returns an array of strings. modifies by reference a number of lines
-char** readfile(char **words, const char *inputfile, int *numberoflines) {
+char** readfile(char **words, char *inputfile, int *numberoflines) {
+
+    char  filename[100];                               // Declares new string            
+    strcpy(filename,inputfile);                        // Copys the string to preserve orignal string
+    strcat(filename,".in");                            // Adds  extension to file name
 
     FILE *file;                                        // Alocate file memory
-    file = fopen(inputfile, "r");                      // Open file with read permission
+    file = fopen(filename, "r");                       // Open file with read permission
     if (file == NULL)                                  // If no file
     {   
         *numberoflines = 0;                                // Set number of lines to 0
@@ -116,4 +128,25 @@ char** readfile(char **words, const char *inputfile, int *numberoflines) {
 
     fclose(file);                                     // Closes file
     return words;                                     // Returns number of lines read
+}
+
+// Write a file
+int writefile(char **words, char *outputfile, int *numberoflines) {
+
+    char  filename[100];                               // Declares new string            
+    strcpy(filename,outputfile);                       // Copys the string to preserve orignal string
+    strcat(filename,".out");                           // Adds  extension to file name
+ 
+    FILE *file;                                        // Alocate file memory
+    file = fopen(filename, "w");                       // Open file with write permission
+    if (file == NULL) return -1;                       // If no file return -1
+
+    for (int i = 0; i < *numberoflines; i++)           // For every line
+    {
+        fprintf(file, "Case #%d: %s\n",i+1,words[i]);                        
+    } 
+
+    printf("File  %s, written with sucess! \n", outputfile);
+    return 0;
+
 }
